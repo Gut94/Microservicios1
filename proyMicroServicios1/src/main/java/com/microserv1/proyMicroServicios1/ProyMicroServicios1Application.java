09388cdca8607acb27777b199da676a4e023645f
ranavarr <raul.navarro-lopez-de-hierro-external@capgemini.com>
2020-03-02 12:52:51 +0100

    prueba commit

--- proyMicroServicios1/src/main/java/com/microserv1/proyMicroServicios1/ProyMicroServicios1Application.java
+++ proyMicroServicios1/src/main/java/com/microserv1/proyMicroServicios1/ProyMicroServicios1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyMicroServicios1Application {
+public class ProyMicroServicios1Application {//arranca la aplicacion
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyMicroServicios1Application.class, args);
