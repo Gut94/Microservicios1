4403261a1b7ba66e105c5add00f4f2c5a2bbd7e9
ranavarr <raul.navarro-lopez-de-hierro-external@capgemini.com>
2020-03-02 12:59:22 +0100

    nueva rama

--- proyMicroServicios1/src/main/java/com/microserv1/proyMicroServicios1/datos/ParkingDaoImpl.java
+++ proyMicroServicios1/src/main/java/com/microserv1/proyMicroServicios1/datos/ParkingDaoImpl.java
@@ -16,7 +16,7 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
-
+//buscar otra forma
 @Repository
 public class ParkingDaoImpl implements ParkingDao {
 
